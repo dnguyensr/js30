b872c4b7bcff19409274a469045c85f28f638b03
Daniel <danielnguyensr@gmail.com>
2017-04-22 12:35:51 -0700

    Add reminaing exercises

---
+++ 20-SpeechDetection/index.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+  window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+  const recognition = new SpeechRecognition();
+  recognition.interimResults = true;  
+
+  let p = document.createElement('p');
+  const words = document.querySelector('.words');
+  words.appendChild(p);
+
+  recognition.addEventListener('result', e => {
+    const transcript = Array.from(e.results)
+  });
+
+  recognition.start();
+}
---
+++ 21-Geolocation/index.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+
+  const arrow = document.querySelector('.arrow');
+  const speed = document.querySelector('.speed-value');
+
+  navigator.geolocation.watchPosition((data) =>{
+    console.log(data);
+  });
+}
